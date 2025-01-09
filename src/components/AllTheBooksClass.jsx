271026084c835d1055074d7ed996c5d43628bd75
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-09 17:04:37 +0100

    search not working

--- src/components/AllTheBooksClass.jsx
+++ src/components/AllTheBooksClass.jsx
@@ -1,4 +1,4 @@
-import { Button, } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 import fantasy from "../data/fantasy.json";
 import history from "../data/history.json";
 import horror from "../data/horror.json";
@@ -8,23 +8,40 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { Component } from "react";
 import BookList from "./BookList";
 
-
 class AllTheBooksClass extends Component {
-
   state = {
-    activeCategory: fantasy,
+    activeCategory: {
+      categoria: fantasy,
+    },
+    searchInput: {
+      research: "",
+    },
   };
-  
-  changeState = function(parameter){
+
+  changeState = function (parameter) {
     this.setState({
-      activeCategory: parameter
-    })
-  }
- 
-  
+      activeCategory: parameter,
+    });
+  };
+
+  checkResearch = function () {
+    if (this.state.searchInput.research === "") {
+      return;
+    } else {
+      const arrayfiltrato = this.state.activeCategory.categoria.filter(
+        (libro) =>
+          libro.title.toLowerCase().includes(this.state.searchInput.research)
+      );
+      // this.setState({
+      //   activeCategory: {
+      //     categoria: arrayfiltrato,
+      //   },
+      // });
+      console.log(arrayfiltrato)
+    }
+  };
 
   render() {
-    
     return (
       <>
         <h1 className=" text-center fw-bolder">Welcome to EpiBooks!</h1>
@@ -35,19 +52,89 @@ class AllTheBooksClass extends Component {
               className=" justify-content-center text-center"
             >
               {/* <Button className="mx-1 btn-secondary" onClick={()=> this.changeState(fantasy)}>Fantasy</Button>  SOTTO MESSO CON UNA FUNZIONE ESTERNA MA PREFERIBILMENTE SI USA LA FUNZIONE FRECCIA*/}
-              <Button className="mx-1 btn-secondary" onClick={()=> this.setState({activeCategory:fantasy})}>Fantasy</Button>     
-              <Button className="mx-1 btn-secondary" onClick={()=> this.setState({activeCategory:history})}>History</Button>
-              <Button className="mx-1 btn-secondary" onClick={()=> this.setState({activeCategory:horror})}>Horror</Button>
-              <Button className="mx-1 btn-secondary" onClick={()=> this.setState({activeCategory:romance})}>Romance</Button>
-              <Button className="mx-1 btn-secondary" onClick={()=> this.setState({activeCategory:scifi})}>Scifi</Button>
+              <Button
+                className="mx-1 btn-secondary"
+                onClick={() =>
+                  this.setState({
+                    activeCategory: { categoria: fantasy },
+                  })
+                }
+              >
+                Fantasy
+              </Button>
+              <Button
+                className="mx-1 btn-secondary"
+                onClick={() =>
+                  this.setState({
+                    activeCategory: {
+                      categoria: history,
+                    },
+                  })
+                }
+              >
+                History
+              </Button>
+              <Button
+                className="mx-1 btn-secondary"
+                onClick={() =>
+                  this.setState({
+                    activeCategory: {
+                      categoria: horror,
+                    },
+                  })
+                }
+              >
+                Horror
+              </Button>
+              <Button
+                className="mx-1 btn-secondary"
+                onClick={() =>
+                  this.setState({
+                    activeCategory: {
+                      categoria: romance,
+                    },
+                  })
+                }
+              >
+                Romance
+              </Button>
+              <Button
+                className="mx-1 btn-secondary"
+                onClick={() =>
+                  this.setState({
+                    activeCategory: {
+                      categoria: scifi,
+                    },
+                  })
+                }
+              >
+                Scifi
+              </Button>
             </ListGroup>
+            <Container className="pt-4">
+              <Form>
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    type="text"
+                    placeholder="Search your book..."
+                    value={this.state.searchInput.research}
+                    onChange={(event) => {
+                      this.setState({
+                        searchInput: { research: event.target.value },
+                      });
+                    }}
+                  />
+                </Form.Group>
+              </Form>
+            </Container>
+            {/* se il formValue e' vuoto fai listaLibri deve essere la active category altrimenti deve essere il risultato del filtro di active category */}
           </div>
-              <BookList listaLibri={this.state.activeCategory}/>
+          {this.checkResearch()}
+          <BookList listaLibri={this.state.activeCategory.categoria} />
         </main>
       </>
     );
   }
 }
-
 
 export default AllTheBooksClass;
